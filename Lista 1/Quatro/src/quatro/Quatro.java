17ecd223cc7f1d1d7c280ce1d4979a5f8ae912ce
EduardoSalesTenorio <eduardosallis30@gmail.com>
2018-08-08 16:20:52 -0300

    Terceira questão finalizada

---
+++ Lista 1/Quatro/src/quatro/Quatro.java
@@ -0,0 +1,25 @@
+/*
+Um banco concederá um crédito especial aos seus clientes, variável com o saldo
+médio no último ano. Faça um algoritmo que leia o saldo médio de um cliente e 
+calcule o valor do crédito de acordo com a tabela abaixo. Mostre uma mensagem 
+informando o saldo médio e o valor do crédito. (use o comando caso-de e não faça
+repetições)
+
+Saldo médio Percentual
+de 0 a 200 nenhum crédito
+de 201 a 400 20% do valor do saldo médio
+de 401 a 600 30% do valor do saldo médio
+acima de 601 40% do valor do saldo médio
+ */
+package quatro;
+
+/**
+ * @author Eduardo Sales
+ */
+public class Quatro {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- Lista 1/Tres/src/tres/Tres.java
+++ Lista 1/Tres/src/tres/Tres.java
@@ -14,14 +14,86 @@ e que tenham olhos verdes e cabelos louros
  */
 package tres;
 
+import javax.swing.JOptionPane;
+
 /**
- *
  * @author Eduardo Sales
  */
 public class Tres {
 
     public static void main(String[] args) {
-       
+
+        Tres a = new Tres();
+
+        String[] sexo = {"M", "F", "M", "M", "M", "F", "F", "F", "M", "M"};
+        String[] olhos = {"Azuis", "Castanhos", "Verdes", "Castanhos", "Castanhos", "Azuis", "Castanhos", "Verdes", "Castanhos", "Castanhos"};
+        String[] cabelos = {"Pretos", "Castanhos", "Loiros", "Loiros", "Castanhos", "Loiros", "Loiros", "Pretos", "Pretos", "Pretos"};
+        int[] idade = {4, 61, 19, 33, 20, 31, 90, 30, 27, 24};
+
+        int maior = 0;
+        int tamanhoPopulacao = 0;
+        int percentual = 0;
+        int resultado;
+
+        maior = a.maior(idade);
+        JOptionPane.showMessageDialog(null, "Habitante mais velho: " + idade[maior]);
+
+        tamanhoPopulacao = a.tamanho(idade);
+
+        percentual = a.percentual(sexo, olhos, cabelos, idade);
+
+        resultado = (percentual * 100) / tamanhoPopulacao;
+        JOptionPane.showMessageDialog(null, "Percentual que atende os requisitos: " + resultado + "%");
+
     }
-    
+
+    int percentual(String[] sexo, String[] olhos, String[] cabelos, int[] idade) {
+        int cont = 0;
+
+        for (int i = 0; i < sexo.length; i++) {
+
+            if ("M".equals(sexo[i])) {
+
+                if ("Verdes".equals(olhos[i])) {
+
+                    if ("Loiros".equals(cabelos[i])) {
+
+                        if (idade[i] > 18 && idade[i] < 35) {
+                            cont++;
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
+        return cont;
+    }
+
+    int tamanho(int[] tamanhoPopulacao) {
+
+        int cont = 0;
+        for (int i = 0; i < tamanhoPopulacao.length; i++) {
+            cont++;
+        }
+
+        return cont;
+    }
+
+    int maior(int[] idade) {
+        int maior = idade[0];
+        int cont = 0;
+
+        for (int i = 0; i < idade.length; i++) {
+
+            if (maior < idade[i]) {
+                maior = idade[i];
+                cont = i;
+            }
+        }
+        return cont;
+    }
+
 }
