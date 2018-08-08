00018df2e684990ce58e212b45c52c3100a53a73
EduardoSalesTenorio <eduardosallis30@gmail.com>
2018-08-08 16:50:54 -0300

    Quarta questão finalizada

--- Lista 1/Quatro/src/quatro/Quatro.java
+++ Lista 1/Quatro/src/quatro/Quatro.java
@@ -13,12 +13,35 @@ acima de 601 40% do valor do saldo médio
  */
 package quatro;
 
+import java.util.Scanner;
+
 /**
  * @author Eduardo Sales
  */
 public class Quatro {
 
     public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        double saldoIni = 0;
+        double resultado = 0;
+
+        System.out.println("Escreva seu saldo");
+        saldoIni = entrada.nextDouble();
+        
+        if (saldoIni <= 200) {
+            System.out.println("Nenhum Credito");
+        } else if (saldoIni > 200 && saldoIni <= 400) {
+            resultado = saldoIni + (saldoIni * 0.2);
+            System.out.println("Seu saldo é de: " + resultado);
+        } else if (saldoIni > 400 && saldoIni <= 600) {
+            resultado = saldoIni + (saldoIni * 0.3);
+            System.out.println("Seu saldo é de: " + resultado);
+        } else if (saldoIni > 600) {
+           resultado = saldoIni + (saldoIni * 0.4);
+            System.out.println("Seu saldo é de: " + resultado);
+        }
 
     }
 
